152028a64bb49a84dba9c10e54954e19543f996d
Manas Ranjan Mallick <manas1104@gmail.com>
2020-04-30 19:04:36 +1000

    Updated

--- dj_cookie_cutter/docs/conf.py
+++ dj_cookie_cutter/docs/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 
-# import django
-# sys.path.insert(0, os.path.abspath('..'))
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-# django.setup()
+import django
+sys.path.insert(0, os.path.abspath('..'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+django.setup()
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +31,9 @@ author = "Manas Mallick"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
